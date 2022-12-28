ef46424e4b4bd5798b57130b0f3c8f09e1c4a553
nagyb3 <nagybence2003@gmail.com>
2022-12-28 21:02:57 +0100

    all done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
 const removeFromArray = function() {
-
+    const argumentArray = Array.from(arguments);
+    let array = argumentArray[0];
+    for (let i = 1; i < argumentArray.length; i++) {
+        if (array.includes(argumentArray[i])) {
+            array.splice(array.indexOf(argumentArray[i]), 1);
+        }
+    }
+    return array
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(number) {
+    if (number % 4 !== 0) {
+        return false
+    } else if (number % 100 === 0) {
+        if (number % 400 === 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+    return true
 };
 
 // Do not edit below this line
